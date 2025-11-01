1f1604df70f83d53b614c4523752020689fe3a90
Soumika Devarakonda <142423331+soumikadevarakonda@users.noreply.github.com>
2025-11-01 10:54:42 +0530

    Update API URL for authentication service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://backend:8080/back1/auth";
+const API_URL = "http://backend:8080/back1/api/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
